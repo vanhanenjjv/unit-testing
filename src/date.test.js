b875fb999d818cdca66bd6fce7c317dffef88ded
Janne Vanhanen <janne.vanhanen@outlook.com>
2022-04-17 15:28:00 +0300

    Add date print tests

--- src/date.test.js
+++ src/date.test.js
@@ -1,7 +1,19 @@
-describe('Date', () => {
-  describe('print', () => {
-    it('should ...', () => {
+const Date = require('./date')
 
+describe('Date', () => {
+  describe('print()', () => {
+    it('should print "1.2.2001"', () => {
+      const consoleLogMock = jest.spyOn(console, 'log').mockImplementation();
+      const date = new Date(1, 2, 2001);
+      date.print();
+      expect(consoleLogMock).toBeCalledWith('1.2.2001');
+    })
+
+    it('should print "5.12.2006"', () => {
+      const consoleLogMock = jest.spyOn(console, 'log').mockImplementation();
+      const date = new Date(5, 12, 2006);
+      date.print();
+      expect(consoleLogMock).toBeCalledWith('5.12.2006');
     })
   })
 })

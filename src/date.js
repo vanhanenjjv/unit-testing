8ab65af2478436b44668936745f795119c749fe3
Tomi Minkkinen <tomi.minkkinen@vincit.fi>
2022-04-17 14:21:04 +0300

    Add isLeapYear method

--- src/date.js
+++ src/date.js
@@ -1,34 +1,35 @@
 module.exports = class Date {
   constructor(day, month, year) {
     if (day === undefined) {
-      this.year = 1900
-      this.month = 1
-      this.day = 1
+      this.year = 1900;
+      this.month = 1;
+      this.day = 1;
     } else {
-      this.day = day
-      this.month = month
-      this.year = year
-
+      this.day = day;
+      this.month = month;
+      this.year = year;
     }
   }
 
-  print() { 
+  print() {}
+
+  nextDay() {}
+
+  get day() {}
+
+  get month() {}
+
+  get year() {}
+
+  #isLeapYear() {
+    if (this.year % 400 === 0) {
+      return true;
+    } else if (this.year % 100 === 0) {
+      return false;
+    } else if (this.year % 4 === 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
-
-  nextDay() {
-
-  }
-
-  get day() {
-  }
-
-  get month() {
-
-  }
-
-  get year() {
-
-  }
-
-
-}
+};

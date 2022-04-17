fa35291103c4582d44f654a6c503fe09bb713b8f
Janne Vanhanen <janne.vanhanen@outlook.com>
2022-04-17 14:31:35 +0300

    cleanup :)

--- src/date.js
+++ src/date.js
@@ -21,15 +21,10 @@ module.exports = class Date {
    * @returns {boolean}
    */
   #isLeapYear() {
-    if (this.year % 400 === 0) {
-      return true;
-    } else if (this.year % 100 === 0) {
-      return false;
-    } else if (this.year % 4 === 0) {
-      return true;
-    } else {
-      return false;
-    }
+    if (this.year % 400 === 0) return true;
+    if (this.year % 100 === 0) return false;
+    if (this.year % 4 === 0) return true;
+    return false;
   }
 
   /**

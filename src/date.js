b40fcdd2fe86fc1b4e809b88991fb329274d4400
Janne Vanhanen <janne.vanhanen@outlook.com>
2022-04-17 14:47:07 +0300

    Implement "days in month" function

--- src/date.js
+++ src/date.js
@@ -28,6 +28,29 @@ module.exports = class Date {
   }
 
   /**
+   * @param {number} month
+   * @param {boolean} isLeapYear
+   * @returns {number}
+   */
+  #daysInMonth(month, isLeapYear) {
+    switch (month) {
+      case 1: return 31;
+      case 2: return isLeapYear ? 29 : 28;
+      case 3: return 31;
+      case 4: return 30;
+      case 5: return 31;
+      case 6: return 30;
+      case 7: return 31;
+      case 8: return 31;
+      case 9: return 30;
+      case 10: return 31;
+      case 11: return 30;
+      case 12: return 31;
+      default: throw new Error('Invalid month');
+    } 
+  } 
+
+  /**
    * @returns {void}
    */
   nextDay() {

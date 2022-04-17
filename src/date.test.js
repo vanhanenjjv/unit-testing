bee93e2fd507f2a7bbc70e8e3ee816bb38674794
Janne Vanhanen <janne.vanhanen@outlook.com>
2022-04-17 15:34:17 +0300

    Add next day tests

--- src/date.test.js
+++ src/date.test.js
@@ -16,4 +16,38 @@ describe('Date', () => {
       expect(consoleLogMock).toBeCalledWith('5.12.2006');
     })
   })
+
+  describe('nextDay()', () => {
+    it('should increment day by one', () => {
+      const date = new Date(1, 2, 2001);
+      date.nextDay();
+      expect(date.day).toBe(2);
+    })
+
+    it('should set day to one and increment month by one', () => {
+      const date = new Date(31, 9, 2001);
+      date.nextDay();
+      expect(date.day).toBe(1);
+      expect(date.month).toBe(10);
+    })
+
+    it('should set day and month to one and increment year by one', () => {
+      const date = new Date(31, 12, 2001);
+      date.nextDay();
+      expect(date.day).toBe(1);
+      expect(date.month).toBe(1);
+      expect(date.year).toBe(2002);
+    })
+
+    it('should take leap years into account', () => {
+      const leapYearDate = new Date(28, 2, 2020);
+      leapYearDate.nextDay();
+      expect(leapYearDate.day).toBe(29);
+
+      const nonLeapYearDate = new Date(28, 2, 2019);
+      nonLeapYearDate.nextDay();
+      expect(nonLeapYearDate.day).toBe(1);
+      expect(nonLeapYearDate.month).toBe(3);
+    })
+  })
 })
